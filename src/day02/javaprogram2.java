71ea75f90996b9d66666eaa0e25e45b7d44c4a30
saidivi <sairam.divi@gmail.com>
2023-05-10 18:31:24 +0530

    day2
    java program- class, main method, static

---
+++ src/day02/javaprogram2.java
@@ -0,0 +1,18 @@
+package day02;
+
+public class javaprogram2 {
+	
+	//main method
+	/*
+	 Today we are seeing package and
+	 class with main method
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Good day!");
+		System.out.println("Good morning!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
